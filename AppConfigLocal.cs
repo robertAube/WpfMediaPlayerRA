4099082057f0e001d56a89ebff5b282fd49c893d
Robert Aubé <robert.aube@cegeplimoilou.ca>
2025-12-04 11:09:52 -0500

    ++

--- AppConfigLocal.cs
+++ AppConfigLocal.cs
@@ -4,7 +4,7 @@ using System.IO;
 namespace WpfMediaPlayerRA {
     public class AppConfigLocal {
         //   public static readonly string EXCEL_MEDIA_LIST = ""; //si n'hexiste pas, prends dans le répertoire courant
-        public static readonly string DEFAULT_EXCEL_NAME = @"A134V3.xlsm";
+        public static readonly string DEFAULT_EXCEL_NAME = @"defaut.xlsm";
         public static readonly string EXCEL_MEDIA_LIST = @"Q:\zulu\Nouveau dossier\A134V 3.xlsm"; //si n'hexiste pas, prends dans le répertoire courant
     }
 }
